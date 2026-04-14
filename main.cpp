40c2dcc1a841d4570aed6c795a53177135d146e4
Prasoon Kandel <prasoonkandel1029@gmail.com>
2026-04-14 18:31:19 +0545

    feat: add program to take and validate input

--- main.cpp
+++ main.cpp
@@ -1,9 +1,46 @@
 #include <iostream>
-
+#include <limits>
 using namespace std;
 
+void margin();
+
 int main(){
-	cout<<"Weather Cpp"<<endl;
+	
+	cout<<"#===========================Weather APP===========================#"<<endl;
+	bool running = true;
+	string cityname;
+	char choice;
+	while(running){
+		cout<<"  Enter the name of city: ";
+		getline(cin, cityname);
+		margin();
+		cout<<"  The name of the city is: "<<cityname<<endl;
+		margin();
+		cout<<"  Do you want to search again (y/n): ";
+		cin>>choice;
+		while(true){
+			if(choice == 'y' || choice == 'Y'){
+			break;
+			}
+			else if(choice == 'n' || choice == 'N'){
+			running = false;
+			break;
+			}
+			else{
+				cin.clear();
+				cin.ignore(numeric_limits<streamsize>::max(),'\n');
+				cout<<"  Invalid Input. Please Try Aagain. "<<endl;
+				cout<<"  Do you want to search again (y/n): ";
+				cin>>choice;
+            }
+		}
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		margin();
+	}
 	return 0;
 
+}
+
+void margin(){
+	cout<<"==================================================================="<<endl;
 }
