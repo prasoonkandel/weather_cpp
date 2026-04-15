83a482d0a2f6c80dbaa6f2a9c3abdd44a2ac10a7
Prasoon Kandel <prasoonkandel1029@gmail.com>
2026-04-15 11:05:39 +0545

    chore: defining essential functions in operations.hpp

--- operations.hpp
+++ operations.hpp
@@ -1,7 +1,16 @@
+#include <string>
+#include <vector>
 #ifndef OPERATIONS_HPP
 #define OPERATIONS_HPP
 
 //Margin Functions
 void margin();
 
-#endif 
+//Various Operations
+std::vector<int> getCords(std::string cityname, std::string countryname);
+std::string fetchAPI(int latitude, int longitude);
+std::string searchCity(std::string cityname, std::string countryname);
+std::vector<std::string> getWeather();
+void displayOutput(std::vector<std::string> &data);
+
+#endif
