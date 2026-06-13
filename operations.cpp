7bdadb038968cc90375d30e8000e11c0a705fde7
Prasoon Kandel <prasoonkandel1029@gmail.com>
2026-06-13 17:35:09 +0545

    feat: complete weatherCode() function in operations.cpp

--- operations.cpp
+++ operations.cpp
@@ -131,4 +131,55 @@ void showWeather(double latitude, double longitude){
     cout<<"  Temperature: "<<temperature<<temperature_unit<<endl;
     cout<<"  Wind Speed: "<<windspeed<<windspeed_unit<<endl;
     cout<<"  Wind Direction: "<<winddirection<<winddirection_unit<<endl;
+    weatherCode(4);
+}
+
+string weatherCode(int code) {
+  switch (code) {
+    case 0:
+      return "Sunny";
+    case 1:
+      return "Mostly Sunny";
+    case 2:
+      return "Partly Cloudy";
+    case 3:
+      return "Cloudy";
+    case 45:
+    case 48:
+      return "Foggy";
+    case 51:
+    case 53:
+    case 55:
+      return "Drizzle";
+    case 56:
+    case 57:
+      return "Freezing Drizzle";
+    case 61:
+    case 63:
+    case 65:
+      return "Rain";
+    case 66:
+    case 67:
+      return "Freezing Rain";
+    case 71:
+    case 73:
+    case 75:
+      return "Snow";
+    case 77:
+      return "Snow Grains";
+    case 80:
+    case 81:
+    case 82:
+      return "Rain Showers";
+    case 85:
+    case 86:
+      return "Snow Showers";
+    case 95:
+      return "Thunderstorm";
+    case 96:
+    case 99:
+      return "Thunderstorm & Hail";
+    default:
+      return "Unknown";
+  }
 }
