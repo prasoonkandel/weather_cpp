1818c8d69f50bbcc8354e08fa68f089386ad20bb
Prasoon Kandel <prasoonkandel1029@gmail.com>
2026-06-29 23:25:29 +0545

    chore: clearing Location related function

--- operations.cpp
+++ operations.cpp
@@ -30,66 +30,6 @@ void loadingClear() {
 
 //Various Operations
 
-string searchCity(string cityname, string countryname) {
-    httplib::Client cli("https://nominatim.openstreetmap.org");
-
-    httplib::Headers headers = {
-        { "User-Agent", "weather-app" },
-        { "Accept-Language", "en" }
-    };
-    string path = "/search?q=" + cityname + "," + countryname + "&format=json";
-
-    auto res = cli.Get(path.c_str(), headers);
-
-    if (!res || res->status != 200) {
-    	loadingClear();
-        cout << "  HTTP error\n";
-        return "";
-    }
-
-    json response = json::parse(res->body);
-
-    if (!response.is_array() || response.empty()) {
-        loadingClear();
-        cout << "  No results\n";
-        return "";
-    }
-
-    return response[0]["display_name"];
-}
-
-void getCords(string cityname, string countryname, vector<double> &cords){
-	httplib::Client cli("https://nominatim.openstreetmap.org");
-	httplib::Headers headers = {
-		{ "User-Agent", "weather-app"},
-		{ "Accept-Language", "en"}
- 	};
-
- 	string path = "/search?q=" + cityname + "," + countryname + "&format=json";
-
- 	auto res = cli.Get(path.c_str(), headers);
-
- 	if (!res || res->status != 200) {
-    	loadingClear();
-        cout<<"\033[1;31m  Error Fetching Coordinates.\033[0m\n";
-        return;
-    }
-
-    json response = json::parse(res->body);
-
-    if(!response.is_array() || response.empty() ){
-    	loadingClear();
-    	cout<<"\033[1;31m  Error Fetching Coordinates.\033[0m\n";
-        return;
-
-    }
-
-    double latitude = stod(response[0]["lat"].get<string>());
-    double longitude = stod(response[0]["lon"].get<string>());
-    cords.push_back(latitude);
-    cords.push_back(longitude);
-}
-
 void showWeather(double latitude, double longitude){
     httplib::Client cli("https://api.open-meteo.com");
     httplib::Headers headers = {
